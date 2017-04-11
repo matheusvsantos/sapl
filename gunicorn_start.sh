28c1f6aa06f66f46b26b1867ddd1e10704f05c6b
Matheus Veleci <matheusveleci@aluno.unb.br>
2017-04-11 14:03:37 -0300

    Adicona configurações para docker-compose.yml

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -2,32 +2,17 @@
 
 # As seen in http://tutos.readthedocs.org/en/latest/source/ndg.html
 
-SAPL_DIR="/var/interlegis/sapl"
-
-# Seta um novo diretório foi passado como raiz para o SAPL
-# caso esse tenha sido passado como parâmetro
-if [ "$1" ]
-then
-    SAPL_DIR="$1"
-fi
-
 NAME="SAPL"                                     # Name of the application (*)
-DJANGODIR=/var/interlegis/sapl                     # Django project directory (*)
-SOCKFILE=/var/interlegis/sapl/run/gunicorn.sock    # we will communicate using this unix socket (*)
+DJANGODIR=/sapl/                    # Django project directory (*)
+SOCKFILE=/sapl/run/gunicorn.sock    # we will communicate using this unix socket (*)
 USER=`whoami`                                   # the user to run as (*)
 GROUP=`whoami`                                  # the group to run as (*)
-NUM_WORKERS=9                                   # how many worker processes should Gunicorn spawn (*)
+NUM_WORKERS=3                                   # how many worker processes should Gunicorn spawn (*)
                                                 # NUM_WORKERS = 2 * CPUS + 1
 DJANGO_SETTINGS_MODULE=sapl.settings            # which settings file should Django use (*)
 DJANGO_WSGI_MODULE=sapl.wsgi                    # WSGI module name (*)
 
-echo "Starting $NAME as `whoami` on base dir $SAPL_DIR"
-
-# Activate the virtual environment
-cd $DJANGODIR
-source /var/interlegis/.virtualenvs/sapl/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+echo "Starting $NAME as `whoami`"
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
